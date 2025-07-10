053d30745c660ec658b1d1c20a75092efac01964
Ayub40 <ayubk4028@gmail.com>
2025-07-10 23:37:21 +0600

    global error handler setup and user controller reorganize

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
 import express, { Request, Response } from "express";
+import cors from "cors"
+// import { UserRoutes } from "./app/modules/user/user.route";
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+// import { envVars } from "./app/config/env";
+// import httpStatus from 'http-status-codes';
+import notFound from "./app/middlewares/notFound";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Backend"
     })
 })
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
 
 export default app;
