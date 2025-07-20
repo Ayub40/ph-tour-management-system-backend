0cbbe60922859b9e062adaae12af968b1851f2d8
Ayub40 <ayubk4028@gmail.com>
2025-07-20 23:29:49 +0600

    search sort filter and pagination added

--- src/app/modules/division/division.model.ts
+++ src/app/modules/division/division.model.ts
@@ -14,7 +14,8 @@ const divisionSchema = new Schema<IDivision>({
 divisionSchema.pre("save", async function (next) {
     if (this.isModified("name")) {
         const baseSlug = this.name.toLowerCase().split(" ").join("-")
-        let slug = `${baseSlug}-division`
+        // let slug = `${baseSlug}-division`
+        let slug = `${baseSlug}`
 
         let counter = 0;
         while (await Division.exists({ slug })) {
