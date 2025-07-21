36ff9b457248d81454291e888993eebe9ba1f5d0
Ayub40 <ayubk4028@gmail.com>
2025-07-22 01:07:49 +0600

    createBooking function has been added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import { UserRoutes } from "../modules/user/user.route"
 import { AuthRoutes } from "../modules/auth/auth.route"
+import { UserRoutes } from "../modules/user/user.route"
 import { TourRoutes } from "../modules/tour/tour.route"
 import { DivisionRoutes } from "../modules/division/division.route"
 import { BookingRoutes } from "../modules/booking/booking.route"
@@ -27,7 +27,11 @@ const moduleRoutes = [
     {
         path: "/booking",
         route: BookingRoutes
-    }
+    },
+    // {
+    //     path: "/payment",
+    //     route: PaymentRoutes
+    // }
 ]
 
 moduleRoutes.forEach((route) => {
