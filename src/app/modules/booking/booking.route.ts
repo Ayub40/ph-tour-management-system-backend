5f2d5861f7783e8b630e8e774c6e42e9e81a8e70
Ayub40 <ayubk4028@gmail.com>
2025-07-22 02:05:45 +0600

    skip some functionality

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -16,28 +16,28 @@ router.post("/",
 );
 
 // api/v1/booking
-router.get("/",
-    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
-    BookingController.getAllBookings
-);
+// router.get("/",
+//     checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+//     BookingController.getAllBookings
+// );
 
 // api/v1/booking/my-bookings
-router.get("/my-bookings",
-    checkAuth(...Object.values(Role)),
-    BookingController.getUserBookings
-);
+// router.get("/my-bookings",
+//     checkAuth(...Object.values(Role)),
+//     BookingController.getUserBookings
+// );
 
 // api/v1/booking/bookingId
-router.get("/:bookingId",
-    checkAuth(...Object.values(Role)),
-    BookingController.getSingleBooking
-);
+// router.get("/:bookingId",
+//     checkAuth(...Object.values(Role)),
+//     BookingController.getSingleBooking
+// );
 
 // api/v1/booking/bookingId/status
-router.patch("/:bookingId/status",
-    checkAuth(...Object.values(Role)),
-    validateRequest(updateBookingStatusZodSchema),
-    BookingController.updateBookingStatus
-);
+// router.patch("/:bookingId/status",
+//     checkAuth(...Object.values(Role)),
+//     validateRequest(updateBookingStatusZodSchema),
+//     BookingController.updateBookingStatus
+// );
 
 export const BookingRoutes = router;
