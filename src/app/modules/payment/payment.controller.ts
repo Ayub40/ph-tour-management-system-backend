f6c9c9d76c2b7fddb3f3edb65475afa1218d01aa
Ayub40 <ayubk4028@gmail.com>
2025-07-24 01:09:22 +0600

    Cancel payment added in payment.controller.ts file

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -21,9 +21,18 @@ const failPayment = catchAsync(async (req: Request, res: Response) => {
     }
 });
 
+const cancelPayment = catchAsync(async (req: Request, res: Response) => {
+    const query = req.query
+    const result = await PaymentService.cancelPayment(query as Record<string, string>)
+
+    if (!result.success) {
+        res.redirect(`${envVars.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`)
+    }
+});
+
 export const PaymentController = {
     // initPayment,
     successPayment,
     failPayment,
-    // cancelPayment,
+    cancelPayment,
 };
