20a81ec1386a5afe1cb7c128e1cf8f320990f693
Ayub40 <ayubk4028@gmail.com>
2025-07-24 00:56:47 +0600

    Fail payment added in payment.controller.ts file

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -12,9 +12,18 @@ const successPayment = catchAsync(async (req: Request, res: Response) => {
     }
 });
 
+const failPayment = catchAsync(async (req: Request, res: Response) => {
+    const query = req.query
+    const result = await PaymentService.failPayment(query as Record<string, string>)
+
+    if (!result.success) {
+        res.redirect(`${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`)
+    }
+});
+
 export const PaymentController = {
     // initPayment,
     successPayment,
-    // failPayment,
+    failPayment,
     // cancelPayment,
 };
