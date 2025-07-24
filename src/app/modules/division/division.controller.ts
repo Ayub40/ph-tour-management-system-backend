4290ea3edbe18702bc0949c2f46a8e7260e50107
Ayub40 <ayubk4028@gmail.com>
2025-07-25 02:04:14 +0600

    division controller updated for Cloudinary

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -6,9 +6,18 @@ import { DivisionService } from "./division.service";
 import { IDivision } from "./division.interface";
 
 const createDivision = catchAsync(async (req: Request, res: Response) => {
+    // console.log({
+    //     file: req.file,
+    //     body: req.body
+    // });
 
- 
-    const result = await DivisionService.createDivision(req.body);
+    const payload: IDivision = {
+        ...req.body,
+        thumbnail: req.file?.path
+    }
+
+    // const result = await DivisionService.createDivision(req.body);
+    const result = await DivisionService.createDivision(payload);
     sendResponse(res, {
         statusCode: 201,
         success: true,
