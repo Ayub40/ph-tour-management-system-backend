becafc9dd7f331e8f4210103b5e6ac6a60810ccf
Ayub40 <ayubk4028@gmail.com>
2025-07-25 02:05:55 +0600

    tour controller updated for Cloudinary

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -6,8 +6,18 @@ import { TourService } from './tour.service';
 import { ITour } from './tour.interface';
 
 const createTour = catchAsync(async (req: Request, res: Response) => {
- 
-    const result = await TourService.createTour(req.body);
+    // console.log({
+    //     body: req.body,
+    //     files: req.files
+    // });
+
+    const payload: ITour = {
+        ...req.body,
+        images: (req.files as Express.Multer.File[]).map(file => file.path)
+    }
+
+    // const result = await TourService.createTour(req.body);
+    const result = await TourService.createTour(payload);
     sendResponse(res, {
         statusCode: 201,
         success: true,
