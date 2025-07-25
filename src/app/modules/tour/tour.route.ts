da002051f625e7d571e9b342efc7090c52a1b9a9
Ayub40 <ayubk4028@gmail.com>
2025-07-26 02:27:23 +0600

    division service  updated and deleted image for cloudinary

--- src/app/modules/tour/tour.route.ts
+++ src/app/modules/tour/tour.route.ts
@@ -46,6 +46,7 @@ router.post(
 router.patch(
     "/:id",
     checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    multerUpload.array("files"),
     validateRequest(updateTourZodSchema),
     TourController.updateTour
 );
