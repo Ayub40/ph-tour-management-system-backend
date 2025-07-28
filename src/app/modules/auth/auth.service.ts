7f5e2e3db252eac217ea3024391698522c89c3bc
Ayub40 <ayubk4028@gmail.com>
2025-07-28 22:49:17 +0600

    forget password added in auth.controller.ts and auth.service.ts

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -9,9 +9,10 @@ import { User } from '../user/user.model';
 // import { generateToken } from '../../utils/jwt';
 // import { envVars } from '../../config/env';
 import { createNewAccessTokenWithRefreshToken } from '../../utils/userTokens';
-import { JwtPayload } from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { envVars } from '../../config/env';
-import { IAuthProvider } from '../user/user.interface';
+import { IAuthProvider, IsActive } from '../user/user.interface';
+import { sendEmail } from '../../utils/sendEmail';
 
 
 // const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -78,6 +79,49 @@ const resetPassword = async (oldPassword: string, newPassword: string, decodedTo
 
 }
 
+const forgotPassword = async (email: string) => {
+    const isUserExist = await User.findOne({ email });
+
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist")
+    }
+    if (!isUserExist.isVerified) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User is not verified")
+    }
+    if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+        throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+    }
+    if (isUserExist.isDeleted) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User is deleted")
+    }
+
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+
+    const resetToken = jwt.sign(jwtPayload, envVars.JWT_ACCESS_SECRET, {
+        expiresIn: "10m"
+    })
+
+    const resetUILink = `${envVars.FRONTEND_URL}/reset-password?id=${isUserExist._id}&token=${resetToken}`
+
+    sendEmail({
+        to: isUserExist.email,
+        subject: "Password Reset",
+        templateName: "forgetPassword",
+        templateData: {
+            name: isUserExist.name,
+            resetUILink
+        }
+    })
+
+    /**
+     * http://localhost:5173/reset-password?id=687f310c724151eb2fcf0c41&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODdmMzEwYzcyNDE1MWViMmZjZjBjNDEiLCJlbWFpbCI6InNhbWluaXNyYXI2QGdtYWlsLmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzUzMTY2MTM3LCJleHAiOjE3NTMxNjY3Mzd9.LQgXBmyBpEPpAQyPjDNPL4m2xLF4XomfUPfoxeG0MKg
+     */
+}
+
 const setPassword = async (userId: string, plainPassword: string) => {
     const user = await User.findById(userId);
 
@@ -132,5 +176,6 @@ export const AuthServices = {
     getNewAccessToken,
     changePassword,
     setPassword,
+    forgotPassword,
     resetPassword
 }
