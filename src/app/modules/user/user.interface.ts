b3d959b7a0ccca6acb3c49dc947dc58aaee18739
Ayub40 <ayubk4028@gmail.com>
2025-08-01 02:08:44 +0600

    clean code

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -33,11 +33,9 @@ export interface IUser {
     phone?: string;
     picture?: string;
     address?: string;
-    // isDeleted?: string;
     isDeleted?: boolean;
     isActive?: IsActive;
     isVerified?: boolean;
-
     role: Role;
     auths: IAuthProvider[]
     bookings?: Types.ObjectId[]
